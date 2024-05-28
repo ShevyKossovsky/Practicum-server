195f6758b9152909e462e222894c8fb0b27894f5
ShevyKossovsky <s0556773361@gmail.com>
2024-05-28 17:06:14 +0300

    0.1.0

--- Server.Data/DataContext.cs
+++ Server.Data/DataContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Server.Core.Entities;
 using System;
 using System.Collections.Generic;
@@ -11,12 +12,22 @@ namespace Server.Data
 {
     public class DataContext: DbContext
     {
+        private readonly IConfiguration _configuration;
+
+        public DataContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public DbSet<Employee> EmployeesList { get; set; }
         public DbSet <Position> PositionsList { get; set; }
         public DbSet<EmployeePosition> EmployeePositionsList { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=EmployeesDB");
+            // optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=EmployeesDB");
+            var connectionString = _configuration.GetConnectionString("cloud_DB");
+            optionsBuilder.UseSqlServer(connectionString);
+
         }
 
 
